6294c71bb9fd74f2769e8ae90b36126996f8daad
Karl Baum <karl.baum@gmail.com>
2016-01-05 05:44:30 -0500

    added some basic benchmarks

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in coverband.gemspec
 gemspec
+gem 'classifier-reborn', github: 'jekyll/classifier-reborn'
---
+++ test/benchmarks/benchmark.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'classifier-reborn', 'lib'))
+
+require 'coverband'
+require 'benchmark'
+require 'classifier-reborn'
+
+
+Coverband.configure do |config|
+  config.root              = Dir.pwd
+  config.startup_delay     = 0
+  config.percentage        = 100.0
+  config.logger            = $stdout
+  config.verbose           = false
+end
+
+bm = Benchmark.measure do
+  1000.times do
+    Coverband::Base.instance.sample do
+      b = ClassifierReborn::Bayes.new 'Interesting', 'Uninteresting'
+      b.train_interesting "here are some good words. I hope you love them"
+      b.train_uninteresting "here are some bad words, I hate you"
+      b.classify "I hate bad words and you" # returns 'Uninteresting'
+    end
+  end
+end
+
+
+puts bm
+
