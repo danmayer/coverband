a833af779263267bfc59eaf3dd33d8d525e83df5
Karl Baum <karl.baum@gmail.com>
2016-01-23 15:30:35 -0500

    added logic for filtering data

--- lib/coverband/memory_cache_store.rb
+++ lib/coverband/memory_cache_store.rb
@@ -1,7 +1,32 @@
-class MemoryCacheStore < Struct.new(:cache_store)
+class MemoryCacheStore
+
+  attr_accessor :store
+
+  def initialize(store)
+    @store = store
+    @files_cache = Hash.new
+  end
 
   def store_report files
-    cache_store.store_report files
+    filtered_files = filter(files)
+    store.store_report(filtered_files) if filtered_files.any?
+  end
+
+  private
+
+  def filter(files)
+    files.each_with_object(Hash.new) do |(file, lines), filtered_file_hash|
+      line_cache = @files_cache[file] ||= Set.new
+      lines.reject! do |line|
+        if line_cache.include? line
+          true
+        else
+          line_cache << line
+          false
+        end
+      end
+      filtered_file_hash[file] = lines if lines.any?
+    end
   end
 
 end
--- test/unit/memory_cache_store_test.rb
+++ test/unit/memory_cache_store_test.rb
@@ -4,19 +4,44 @@ require 'pry-byebug'
 class MemoryCacheStoreTest < Test::Unit::TestCase
 
   def setup
-    @cache_store = mock('cache_store')
-    @memory_cache_store = MemoryCacheStore.new(@cache_store)
+    @store = mock('store')
+    @memory_store = MemoryCacheStore.new(@store)
   end
 
 
-  test 'it passes data into cache store' do
+  test 'it passes data into store' do
     data = {
       'file1' => [ 3, 5 ],
       'file2' => [1, 2]
     }
-    @cache_store.expects(:store_report).with data
-    @memory_cache_store.store_report data
+    @store.expects(:store_report).with data
+    @memory_store.store_report data
   end
 
+  test 'it passes data into store only once' do
+    data = {
+      'file1' => [ 3, 5 ],
+      'file2' => [1, 2]
+    }
+    @store.expects(:store_report).once.with data
+    2.times { @memory_store.store_report data }
+  end
+
+  test 'it only passes files and lines we have not hit yet' do
+    first_data = {
+      'file1' => [ 3, 5 ],
+      'file2' => [1, 2]
+    }
+    second_data = {
+      'file1' => [ 3, 5, 10 ],
+      'file2' => [1, 2]
+    }
+    @store.expects(:store_report).once.with first_data
+    @store.expects(:store_report).once.with(
+      'file1' => [10]
+    )
+    @memory_store.store_report first_data
+    @memory_store.store_report second_data
+  end
 
 end
