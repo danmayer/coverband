748a65a6c2309bb046ce7cd132a62ab02fec3f07
Dan Mayer <dan.mayer@offgrid-electric.com>
2017-07-23 12:24:39 -0600

    fix for travis

--- test/unit/s3_report_writer_test.rb
+++ test/unit/s3_report_writer_test.rb
@@ -11,6 +11,7 @@ module Coverband
       object = mock('object')
       s3.expects(:bucket).with('coverage-bucket').returns(bucket)
       bucket.expects(:object).with('coverband/index.html').returns(object)
+      File.expects(:read).at_least(0) { "content ./assets/#{Gem::Specification.find_by_name('simplecov-html').version.version}/" }
       File.expects(:read).with("#{SimpleCov.coverage_dir}/index.html").returns("content ./assets/#{Gem::Specification.find_by_name('simplecov-html').version.version}/")
       object.expects(:put).with(body: 'content ')
       Aws::S3::Resource.expects(:new).returns(s3)
