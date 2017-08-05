86a876398d7594b597a5cfd3126c94bb0726e619
Dan Mayer <dan.mayer@offgrid-electric.com>
2017-08-05 10:09:37 -0600

    add support for additional coverage files

--- lib/coverband/tasks.rb
+++ lib/coverband/tasks.rb
@@ -1,44 +1,51 @@
 namespace :coverband do
 
+  def safely_import_files(files_to_cover)
+    if files_to_cover.any?
+      files = Coverband::Baseline.exclude_files(files_to_cover)
+      files.each do |file|
+        begin
+          require_dependency file
+        rescue Exception => err
+          if Coverband.configuration.verbose
+            Coverband.configuration.logger.info "error adding file to baseline: #{file}"
+            Coverband.configuration.logger.info "error: #{err}"
+          end
+        end
+      end
+    end
+  end
+
   desc "record coverband coverage baseline"
   task :baseline do
-    Coverband::Baseline.record {
-        if Rake::Task.tasks.any?{ |key| key.to_s.match(/environment$/) }
-          Coverband.configuration.logger.info "invoking rake environment"
-          Rake::Task['environment'].invoke
-        elsif Rake::Task.tasks.any?{ |key| key.to_s.match(/env$/) }
-          Coverband.configuration.logger.info "invoking rake env"
-          Rake::Task["env"].invoke
-        end
+    Coverband::Baseline.record do
+      if Rake::Task.tasks.any?{ |key| key.to_s.match(/environment$/) }
+        Coverband.configuration.logger.info "invoking rake environment"
+        Rake::Task['environment'].invoke
+      elsif Rake::Task.tasks.any?{ |key| key.to_s.match(/env$/) }
+        Coverband.configuration.logger.info "invoking rake env"
+        Rake::Task["env"].invoke
+      end
 
-        baseline_files = [File.expand_path('./config/boot.rb',  Dir.pwd),
-                          File.expand_path('./config/application.rb', Dir.pwd),
-                          File.expand_path('./config/environment.rb', Dir.pwd)]
+      baseline_files = [File.expand_path('./config/boot.rb',  Dir.pwd),
+        File.expand_path('./config/application.rb', Dir.pwd),
+        File.expand_path('./config/environment.rb', Dir.pwd)]
 
-        baseline_files.each do |baseline_file|
-          if File.exists?(baseline_file)
-            require baseline_file
-          end
+      baseline_files.each do |baseline_file|
+        if File.exists?(baseline_file)
+          require baseline_file
         end
-        if defined? Rails
-          files = Coverband::Baseline.exclude_files(Dir.glob("#{Rails.root}/app/**/*.rb"))
-          files.sort.each { |file|
-              begin
-                require_dependency file
-              rescue Exception
-                #ignore
-              end }
-          if File.exists?("#{Rails.root}/lib")
-            files = Coverband::Baseline.exclude_files(Dir.glob("#{Rails.root}/lib/**/*.rb"))
-            files.sort.each { |file|
-              begin
-                require_dependency file
-              rescue Exception
-                #ignoring file
-              end}
-          end
+      end
+
+      safely_import_files(Coverband.configuration.additional_files.flatten)
+
+      if defined? Rails
+        safely_import_files(Dir.glob("#{Rails.root}/app/**/*.rb"))
+        if File.exists?("#{Rails.root}/lib")
+          safely_import_files(Dir.glob("#{Rails.root}/lib/**/*.rb"))
         end
-      }
+      end
+    end
   end
 
   ###
