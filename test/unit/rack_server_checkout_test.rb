891ae45a357c7a6813f6d01c48b369a44b2c0d36
Karl Baum <karl.baum@gmail.com>
2018-11-13 21:18:03 -0500

    Check for rails server command
    Noticed that when starting with rails s on a rails 4 project for some
    reason rack was not in the stack. This pull request looks for the rails
    commands/commands_tasks server method. If it finds this method it delays
    early start and waits until the first rack request.

--- test/unit/rack_server_checkout_test.rb
+++ test/unit/rack_server_checkout_test.rb
@@ -5,14 +5,20 @@ require File.expand_path('../test_helper', File.dirname(__FILE__))
 class RackServerCheckTest < Test::Unit::TestCase
 
   test 'returns true when running in rack server' do
-    caller_locations = ['blah/lib/rack/server.rb'].map{ |path| OpenStruct.new(path: path) }
+    caller_locations = ['blah/lib/rack/server.rb'].map{ |path| OpenStruct.new(path: path, label: 'foo') }
     Kernel.expects(:caller_locations).returns(caller_locations)
     assert_true(Coverband::RackServerCheck.running?)
   end
 
   test 'returns false when not running in rack server' do
-    caller_locations = ['blah/lib/sidekiq/worker.rb'].map{ |path| OpenStruct.new(path: path) }
+    caller_locations = ['blah/lib/sidekiq/worker.rb'].map{ |path| OpenStruct.new(path: path, label: 'foo') }
     Kernel.expects(:caller_locations).returns(caller_locations)
     assert_false(Coverband::RackServerCheck.running?)
+  end
+
+  test 'returns true if running within a rails server' do
+    caller_locations = [OpenStruct.new(path: '/lib/rails/commands/commands_tasks.rb', label: 'server')] 
+    Kernel.expects(:caller_locations).returns(caller_locations)
+    assert_true(Coverband::RackServerCheck.running?)
   end
 end
