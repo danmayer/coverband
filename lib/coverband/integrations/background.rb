675148eef2f861c17afb50a281588cdc94c8adb6
Karl Baum <karl.baum@gmail.com>
2018-11-14 19:03:15 -0500

    Report data to coverband at exit

--- lib/coverband/integrations/background.rb
+++ lib/coverband/integrations/background.rb
@@ -7,18 +7,23 @@ module Coverband
     def self.start
       return if @background_reporting_running
 
+      logger = Coverband.configuration.logger
       @semaphore.synchronize do
         return if @background_reporting_running
 
         @background_reporting_running = true
         sleep_seconds = Coverband.configuration.background_reporting_sleep_seconds
-        logger = Coverband.configuration.logger
         Thread.new do
           loop do
             Coverband::Collectors::Coverage.instance.report_coverage
             logger&.debug("Reported coverage from thread. Sleeping for #{sleep_seconds} seconds")
             sleep(sleep_seconds)
           end
+        end
+
+        at_exit do
+          Coverband::Collectors::Coverage.instance.report_coverage
+          logger&.debug("Reported coverage before exit")
         end
       end
     end
