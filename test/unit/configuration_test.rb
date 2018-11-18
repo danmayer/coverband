ed9dfa75fbe496327b114f02dea34e1828a322f4
Karl Baum <karl.baum@gmail.com>
2018-11-18 07:14:30 -0500

    Move configuration out of test helper
    configuration_test was failing due to rails5 coverband configuration running.

--- test/unit/configuration_test.rb
+++ test/unit/configuration_test.rb
@@ -3,6 +3,18 @@
 require File.expand_path('../test_helper', File.dirname(__FILE__))
 
 class BaseTest < Test::Unit::TestCase
+  def setup
+    Coverband.configure do |config|
+      config.root                = Dir.pwd
+      config.s3_bucket           = nil
+      config.root_paths          = ['/app_path/']
+      config.ignore              = ['vendor']
+      config.reporting_frequency = 100.0
+      config.reporter            = 'std_out'
+      config.store               = Coverband::Adapters::RedisStore.new(Redis.new)
+    end
+  end
+
   test 'defaults ' do
     coverband = Coverband::Collectors::Coverage.instance.reset_instance
     assert_equal ['vendor', 'internal:prelude', 'schema.rb'], coverband.instance_variable_get('@ignore_patterns')
