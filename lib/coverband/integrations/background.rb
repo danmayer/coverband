8dd392744d01522ada6b55c42db7f3312df0a640
Dan Mayer <danmayer@gmail.com>
2018-12-03 20:50:52 -0700

    enable ability to ignore coverband background logs

--- lib/coverband/integrations/background.rb
+++ lib/coverband/integrations/background.rb
@@ -10,21 +10,21 @@ module Coverband
       logger = Coverband.configuration.logger
       @semaphore.synchronize do
         return if @background_reporting_running
-        logger&.debug('Starting background reporting')
+        logger&.debug('Coverband: Starting background reporting')
 
         @background_reporting_running = true
         sleep_seconds = Coverband.configuration.background_reporting_sleep_seconds
         Thread.new do
           loop do
             Coverband::Collectors::Coverage.instance.report_coverage(true)
-            logger&.debug("Reported coverage from thread. Sleeping for #{sleep_seconds} seconds")
+            logger&.debug("Coverband: Reported coverage via thread. Sleeping #{sleep_seconds}s") if Coverband.configuration.verbose
             sleep(sleep_seconds)
           end
         end
 
         at_exit do
           Coverband::Collectors::Coverage.instance.report_coverage(true)
-          logger&.debug('Reported coverage before exit')
+          logger&.debug('Coverband: Reported coverage before exit')
         end
       end
     end
