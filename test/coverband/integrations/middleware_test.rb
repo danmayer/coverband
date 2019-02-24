39c559bb08fe86be5217bd6d982cab5c533dcf9f
Karl Baum <karl.baum@gmail.com>
2019-02-23 20:05:51 -0500

    Move integration tests

--- test/coverband/integrations/middleware_test.rb
+++ test/coverband/integrations/middleware_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.expand_path('../test_helper', File.dirname(__FILE__))
+require File.expand_path('../../test_helper', File.dirname(__FILE__))
 require 'rack'
 
 class MiddlewareTest < Minitest::Test
