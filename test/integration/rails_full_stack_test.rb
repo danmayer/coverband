4069fa5eae077ce8f1ed2c32ba2f6edd1895a1a2
Karl Baum <karl.baum@gmail.com>
2019-03-17 13:44:36 -0400

    Combine rails full stack test into one test
    We can only load rails once per test run unless we start forking for
    these tests.

--- test/integration/rails_full_stack_test.rb
+++ test/integration/rails_full_stack_test.rb
@@ -9,6 +9,8 @@ class RailsFullStackTest < Minitest::Test
   def setup
     super
     rails_setup
+    require 'rainbow'
+    Rainbow('this text is red').red
   end
 
   def teardown
@@ -19,7 +21,7 @@ class RailsFullStackTest < Minitest::Test
 
   # We have to combine everything in one test
   # because we can only initialize rails once per test
-  # run. Possibly fork test runs to avoid this problem?
+  # run. Possibly fork test runs to avoid this problem in future?
   test 'this is how we do it' do
     visit '/dummy/show'
     Coverband.report_coverage(true)
@@ -28,9 +30,13 @@ class RailsFullStackTest < Minitest::Test
     within page.find('a', text: /dummy_controller.rb/).find(:xpath, '../..') do
       assert_selector('td', text: '100.0 %')
     end
+    
+    #Test gems are reporting coverage
+    assert_content('Gems')
+    assert page.html.match('rainbow/wrapper.rb')
 
+    #Test eager load data stored separately
     dummy_controller = "./test/rails#{Rails::VERSION::MAJOR}_dummy/app/controllers/dummy_controller.rb"
-
     store.type = :eager_loading
     eager_expected = [1, 1, 0, nil, nil]
     results = store.coverage[dummy_controller]['data']
@@ -39,7 +45,6 @@ class RailsFullStackTest < Minitest::Test
     store.type = nil
     runtime_expected = [0, 0, 1, nil, nil]
     results = store.coverage[dummy_controller]['data']
-
   end
 
   ###
