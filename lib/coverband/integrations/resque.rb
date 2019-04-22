a1697b6d4b8ecf6dacf9594700e2554f0a9a88af
Dan Mayer <dan.mayer@stitchfix.com>
2019-04-22 16:09:36 -0600

    fix for resque boot storing runtime coverage

--- lib/coverband/integrations/resque.rb
+++ lib/coverband/integrations/resque.rb
@@ -2,9 +2,11 @@
 
 Resque.after_fork do |job|
   Coverband.start
+  Coverband.runtime_coverage!
 end
 
 Resque.before_first_fork do
+  Coverband.eager_loading_coverage!
   Coverband.configuration.background_reporting_enabled = false
   Coverband::Background.stop
   Coverband::Collectors::Coverage.instance.report_coverage(true)
