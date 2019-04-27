a3a773b33533d15da34b9bc04824f59745f60f6b
Karl Baum <karl.baum@gmail.com>
2019-04-27 17:01:06 -0400

    Do not run memory test at all to avoid setup running twice
    Running setup twice causes failure depending on the order of the tests.
    This is to be expected since rails can only eagerly load 1 time.

--- test/integration/rails_full_stack_test.rb
+++ test/integration/rails_full_stack_test.rb
@@ -54,42 +54,44 @@ class RailsFullStackTest < Minitest::Test
   # as we run it in single test mode via the benchmarks.
   # Add new tests below this test
   ###
-  test 'memory usage' do
-    return unless ENV['COVERBAND_MEMORY_TEST']
-    # we don't want this to run during our standard test suite
-    # as the below profiler changes the runtime
-    # and shold only be included for isolated processes
-    begin
-      require 'memory_profiler'
+  if ENV['COVERBAND_MEMORY_TEST']
+    test 'memory usage' do
+      return unless ENV['COVERBAND_MEMORY_TEST']
+      # we don't want this to run during our standard test suite
+      # as the below profiler changes the runtime
+      # and shold only be included for isolated processes
+      begin
+        require 'memory_profiler'
 
-      # warmup
-      3.times do
-        visit '/dummy/show'
-        assert_content('I am no dummy')
-        Coverband.report_coverage(true)
-      end
-
-      previous_out = $stdout
-      capture = StringIO.new
-      $stdout = capture
-
-      MemoryProfiler.report do
-        15.times do
+        # warmup
+        3.times do
           visit '/dummy/show'
           assert_content('I am no dummy')
           Coverband.report_coverage(true)
-          # this is expected to retain memory across requests
-          # clear it to remove the false positive from test
-          Coverband::Collectors::Coverage.instance.send(:add_previous_results, nil)
         end
-      end.pretty_print
-      data = $stdout.string
-      $stdout = previous_out
-      if data.match(/retained objects by gem(.*)retained objects by file/m)[0]&.match(/coverband/)
-        raise 'leaking memory!!!'
+
+        previous_out = $stdout
+        capture = StringIO.new
+        $stdout = capture
+
+        MemoryProfiler.report do
+          15.times do
+            visit '/dummy/show'
+            assert_content('I am no dummy')
+            Coverband.report_coverage(true)
+            # this is expected to retain memory across requests
+            # clear it to remove the false positive from test
+            Coverband::Collectors::Coverage.instance.send(:add_previous_results, nil)
+          end
+        end.pretty_print
+        data = $stdout.string
+        $stdout = previous_out
+        if data.match(/retained objects by gem(.*)retained objects by file/m)[0]&.match(/coverband/)
+          raise 'leaking memory!!!'
+        end
+      ensure
+        $stdout = previous_out
       end
-    ensure
-      $stdout = previous_out
     end
   end
 end
