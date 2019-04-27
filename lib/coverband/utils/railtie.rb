118151672163333efdb013ce23bcc545c667cad9
Karl Baum <karl.baum@gmail.com>
2019-04-27 15:45:51 -0400

    Set to eager_loading when rails eager_load! called

--- lib/coverband/utils/railtie.rb
+++ lib/coverband/utils/railtie.rb
@@ -2,6 +2,18 @@
 
 Coverband.eager_loading_coverage!
 module Coverband
+  module RailsEagerLoad
+    def eager_load!
+      Coverband.configuration.logger&.debug('Coverband: set to eager_loading')
+      Coverband.eager_loading_coverage!
+      super
+    ensure
+      Coverband.report_coverage(true)
+      Coverband.runtime_coverage!
+    end
+  end
+  Rails::Engine.prepend(RailsEagerLoad)
+
   class Railtie < Rails::Railtie
     initializer 'coverband.configure' do |app|
       app.middleware.use Coverband::Middleware
