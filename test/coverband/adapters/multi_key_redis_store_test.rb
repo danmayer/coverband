36782bea9c416b1bbc4657a9c5d2c9051f20a03f
Karl Baum <karl.baum@gmail.com>
2019-06-10 22:28:26 -0400

    Coverage can take local type

--- test/coverband/adapters/multi_key_redis_store_test.rb
+++ test/coverband/adapters/multi_key_redis_store_test.rb
@@ -92,6 +92,16 @@ class MultiKeyRedisStoreTest < Minitest::Test
     assert_equal [1, 2, 0, 1, 5], @store.coverage['app_path/cat.rb']['data']
   end
 
+  def test_coverage_type
+    @store.type = :eager_loading
+    data = {
+      'app_path/dog.rb' => [0, nil, 1, 2]
+    }
+    @store.save_report(data)
+    @store.type = nil
+    assert_equal [0, nil, 1, 2], @store.coverage(:eager_loading)['app_path/dog.rb']['data']
+  end
+
   def test_clear
     @store.type = :eager_loading
     data = {
