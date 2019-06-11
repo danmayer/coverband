099ceba04a4ea23c68b1b2291e54158f9320cb1f
Karl Baum <karl.baum@gmail.com>
2019-06-11 13:14:58 -0400

    Use relative files as keys

--- test/coverband/adapters/multi_key_redis_store_test.rb
+++ test/coverband/adapters/multi_key_redis_store_test.rb
@@ -7,6 +7,10 @@ class MultiKeyRedisStoreTest < Minitest::Test
     super
     @redis = Redis.new
     @redis.flushdb
+    # FIXME: remove dependency on configuration and instead pass this in as an argument
+    Coverband.configure do |config|
+      config.root_paths = ['app_path/']
+    end
     @store = Coverband::Adapters::MultiKeyRedisStore.new(@redis, redis_namespace: 'coverband_test')
     Coverband.configuration.store = @store
     mock_file_hash
@@ -18,7 +22,7 @@ class MultiKeyRedisStoreTest < Minitest::Test
     @store.save_report(
       'app_path/dog.rb' => [0, 1, 2]
     )
-    assert_equal example_line, @store.coverage['app_path/dog.rb']['data']
+    assert_equal example_line, @store.coverage['./dog.rb']['data']
     assert_equal(
       {
         'first_updated_at' => @current_time.to_i,
@@ -26,19 +30,19 @@ class MultiKeyRedisStoreTest < Minitest::Test
         'file_hash' => 'abcd',
         'data' => [0, 1, 2]
       }.to_json,
-      @redis.get('coverband_3_2.coverband_test.app_path/dog.rb')
+      @redis.get('coverband_3_2.coverband_test../dog.rb')
     )
     @store.save_report(
-      'app_path/dog.rb' => [1, 0, 0]
+      'app_path/dog.rb' => [1, 1, 0]
     )
     assert_equal(
       {
         'first_updated_at' => @current_time.to_i,
         'last_updated_at' => @current_time.to_i,
         'file_hash' => 'abcd',
-        'data' => [1, 1, 2]
+        'data' => [1, 2, 2]
       }.to_json,
-      @redis.get('coverband_3_2.coverband_test.app_path/dog.rb')
+      @redis.get('coverband_3_2.coverband_test../dog.rb')
     )
   end
 
@@ -56,10 +60,10 @@ class MultiKeyRedisStoreTest < Minitest::Test
         'last_updated_at' => @current_time.to_i,
         'file_hash' => 'abcd',
         'data' => [0, nil, 1, 2]
-      }, @store.coverage['app_path/dog.rb']
+      }, @store.coverage['./dog.rb']
     )
-    assert_equal [1, 2, 0, 1, 5], @store.coverage['app_path/cat.rb']['data']
-    assert_equal [1, 5, nil, 2], @store.coverage['app_path/ferrit.rb']['data']
+    assert_equal [1, 2, 0, 1, 5], @store.coverage['./cat.rb']['data']
+    assert_equal [1, 5, nil, 2], @store.coverage['./ferrit.rb']['data']
   end
 
   def test_coverage_subset
@@ -69,10 +73,10 @@ class MultiKeyRedisStoreTest < Minitest::Test
       'app_path/ferrit.rb' => [1, 5, nil, 2]
     }
     @store.save_report(data)
-    coverage = @store.coverage(files: ['app_path/cat.rb', 'app_path/ferrit.rb'])
+    coverage = @store.coverage(files: ['./cat.rb', './ferrit.rb'])
     assert_equal 2, coverage.length
-    assert_equal [1, 2, 0, 1, 5], @store.coverage['app_path/cat.rb']['data']
-    assert_equal [1, 5, nil, 2], @store.coverage['app_path/ferrit.rb']['data']
+    assert_equal [1, 2, 0, 1, 5], @store.coverage['./cat.rb']['data']
+    assert_equal [1, 5, nil, 2], @store.coverage['./ferrit.rb']['data']
   end
 
   def test_type
@@ -82,14 +86,14 @@ class MultiKeyRedisStoreTest < Minitest::Test
     }
     @store.save_report(data)
     assert_equal 1, @store.coverage.length
-    assert_equal [0, nil, 1, 2], @store.coverage['app_path/dog.rb']['data']
+    assert_equal [0, nil, 1, 2], @store.coverage['./dog.rb']['data']
     @store.type = nil
     data = {
       'app_path/cat.rb' => [1, 2, 0, 1, 5]
     }
     @store.save_report(data)
     assert_equal 1, @store.coverage.length
-    assert_equal [1, 2, 0, 1, 5], @store.coverage['app_path/cat.rb']['data']
+    assert_equal [1, 2, 0, 1, 5], @store.coverage['./cat.rb']['data']
   end
 
   def test_coverage_type
@@ -99,7 +103,7 @@ class MultiKeyRedisStoreTest < Minitest::Test
     }
     @store.save_report(data)
     @store.type = nil
-    assert_equal [0, nil, 1, 2], @store.coverage(:eager_loading)['app_path/dog.rb']['data']
+    assert_equal [0, nil, 1, 2], @store.coverage(:eager_loading)['./dog.rb']['data']
   end
 
   def test_clear
