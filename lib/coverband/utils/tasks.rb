a58c3bcab328ff6e4e93122e7c901f71b7619a65
Dan Mayer <dan.mayer@stitchfix.com>
2019-06-13 11:54:09 -0600

    remove unneeded global method that could cause issues

--- lib/coverband/utils/tasks.rb
+++ lib/coverband/utils/tasks.rb
@@ -4,15 +4,8 @@ namespace :coverband do
   # handles configuring in require => false and COVERBAND_DISABLE_AUTO_START cases
   Coverband.configure unless Coverband.configured?
 
-  def environment
-    Coverband.configuration.report_on_exit = false
-    Coverband.configuration.background_reporting_enabled = false
-    Rake.application['environment'].invoke if Rake::Task.task_defined?('environment')
-  end
-
   desc 'report runtime Coverband code coverage'
   task :coverage do
-    environment
     if Coverband.configuration.reporter == 'scov'
       Coverband::Reporters::HTMLReport.new(Coverband.configuration.store).report
     else
@@ -22,7 +15,6 @@ namespace :coverband do
 
   desc 'report runtime Coverband code coverage'
   task :coverage_server do
-    environment
     Rack::Server.start app: Coverband::Reporters::Web.new, Port: ENV.fetch('COVERBAND_COVERAGE_PORT', 1022).to_i
   end
 
@@ -31,16 +23,14 @@ namespace :coverband do
   ###
   desc 'reset Coverband coverage data, helpful for development, debugging, etc'
   task :clear do
-    environment
     Coverband.configuration.store.clear!
   end
 
   ###
-  # clear data helpful for development or after configuration issues
+  # Updates the data in the coverband store from one format to another
   ###
   desc 'upgrade previous Coverband datastore to latest format'
   task :migrate do
-    environment
     Coverband.configuration.store.migrate!
   end
 end
