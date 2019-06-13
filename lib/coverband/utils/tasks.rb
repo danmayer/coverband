0a144a4f7a77556775175cafe8e2421564ad16a1
Chris Salzberg <chris@dejimata.com>
2019-06-13 16:47:16 +0900

    Encapsulate environment setup within rake task
    Defining methods in a rake task actually defines them on the global
    namespace, which can clobber other methods or cause other problems.
    Instead of doing that, define a proc in a closure so it can be called
    from within other blocks, without polluting anything global.

--- lib/coverband/utils/tasks.rb
+++ lib/coverband/utils/tasks.rb
@@ -4,15 +4,15 @@ namespace :coverband do
   # handles configuring in require => false and COVERBAND_DISABLE_AUTO_START cases
   Coverband.configure unless Coverband.configured?
 
-  def environment
+  environment = -> {
     Coverband.configuration.report_on_exit = false
     Coverband.configuration.background_reporting_enabled = false
     Rake.application['environment'].invoke if Rake::Task.task_defined?('environment')
-  end
+  }
 
   desc 'report runtime Coverband code coverage'
   task :coverage do
-    environment
+    environment.call
     if Coverband.configuration.reporter == 'scov'
       Coverband::Reporters::HTMLReport.new(Coverband.configuration.store).report
     else
@@ -22,7 +22,7 @@ namespace :coverband do
 
   desc 'report runtime Coverband code coverage'
   task :coverage_server do
-    environment
+    environment.call
     Rack::Server.start app: Coverband::Reporters::Web.new, Port: ENV.fetch('COVERBAND_COVERAGE_PORT', 1022).to_i
   end
 
@@ -31,7 +31,7 @@ namespace :coverband do
   ###
   desc 'reset Coverband coverage data, helpful for development, debugging, etc'
   task :clear do
-    environment
+    environment.call
     Coverband.configuration.store.clear!
   end
 
@@ -40,7 +40,7 @@ namespace :coverband do
   ###
   desc 'upgrade previous Coverband datastore to latest format'
   task :migrate do
-    environment
+    environment.call
     Coverband.configuration.store.migrate!
   end
 end
