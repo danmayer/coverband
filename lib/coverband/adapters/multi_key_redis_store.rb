d4ef9d178d877ada3a5d27df9109b4e790342783
Karl Baum <karl.baum@gmail.com>
2019-07-20 21:56:52 -0400

    wip

--- lib/coverband/adapters/multi_key_redis_store.rb
+++ lib/coverband/adapters/multi_key_redis_store.rb
@@ -3,17 +3,18 @@
 module Coverband
   module Adapters
     class MultiKeyRedisStore < Base
+      META_DATA_KEYS = [DATA_KEY, FIRST_UPDATED_KEY, LAST_UPDATED_KEY, FILE_HASH].freeze
       ###
       # This key isn't related to the coverband version, but to the interal format
       # used to store data to redis. It is changed only when breaking changes to our
       # redis format are required.
       ###
-      REDIS_STORAGE_FORMAT_VERSION = 'coverband_3_2'
+      REDIS_STORAGE_FORMAT_VERSION = 'coverband_3_3'
 
       def initialize(redis, opts = {})
         super()
         @redis_namespace = opts[:redis_namespace]
-        @format_version  = REDIS_STORAGE_FORMAT_VERSION
+        @format_version = REDIS_STORAGE_FORMAT_VERSION
         @redis = redis
       end
 
@@ -29,18 +30,33 @@ module Coverband
       end
 
       def save_report(report)
-        merged_report = merge_reports(report, coverage(files: report.keys))
-        key_values = merged_report.map do |file, data|
-          [key(file), data.to_json]
-        end.flatten
-        @redis.mset(*key_values) if key_values.any?
-        keys = merged_report.keys
+        report_time = Time.now.to_i
+        updated_time = type == Coverband::EAGER_TYPE ? nil : report_time
+        report.each do |file, data|
+          data.each_with_index do |line_coverage, index|
+            key = key(full_path_to_relative(file))
+            @redis.hincrby(key, index, line_coverage) if line_coverage
+            @redis.hmset(key, FIRST_UPDATED_KEY, report_time, LAST_UPDATED_KEY, updated_time, FILE_HASH, file_hash(file))
+          end
+        end
+        keys = report.keys.map { |file| full_path_to_relative(file) }
         @redis.sadd(files_key, keys) if keys.any?
       end
 
       def coverage(local_type = nil, files: nil)
         files = relative_paths(files) || files_set(local_type)
-        Hash[files.zip(values_from_redis(local_type, files))]
+        files.each_with_object({}) do |file, hash|
+          data_from_redis = @redis.hgetall(key(full_path_to_relative(file), local_type))
+
+          max = (data_from_redis.keys - META_DATA_KEYS).map(&:to_i).max
+          data = (max + 1).times.map do |index|
+            line_coverage = data_from_redis[index.to_s]
+            line_coverage ? line_coverage.to_i : nil
+          end
+          hash[file] = data_from_redis.select { |key, _value| META_DATA_KEYS.include?(key) }.merge!('data' => data)
+          hash[file][LAST_UPDATED_KEY] = hash[file][LAST_UPDATED_KEY].to_i
+          hash[file][FIRST_UPDATED_KEY] = hash[file][FIRST_UPDATED_KEY].to_i
+        end
       end
 
       private
