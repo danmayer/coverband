bbf9fe45402db89e8dea9fd50b16e4e148f69f1d
Karl Baum <karl.baum@gmail.com>
2019-07-25 08:45:54 -0400

    Incr josie by 8

--- lib/lua.rb
+++ lib/lua.rb
@@ -4,6 +4,6 @@ require 'redis'
 
 redis = Redis.new
 script_id = redis.script(:load, <<~LUA)
-  return { KEYS, ARGV }
+  return redis.call("INCRBY", KEYS[1], ARGV[1])
 LUA
-puts redis.evalsha(script_id, ['key'], ['arg'])
+puts redis.evalsha(script_id, ['josie'], ['8'])
