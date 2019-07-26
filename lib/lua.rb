c8734fac3c982f4272742e7a13d6deb59cd4a4dd
Karl Baum <karl.baum@gmail.com>
2019-07-26 10:11:13 -0400

    Reduce to one script call to set hash

--- lib/lua.rb
+++ lib/lua.rb
@@ -4,13 +4,30 @@ require 'redis'
 
 redis = Redis.new
 script_id = redis.script(:load, <<~LUA)
-  for i, values in ipairs(ARGV) do
+  local first_updated_at = table.remove(ARGV, 1)
+  local last_updated_at = table.remove(ARGV, 1)
+  local file_hash = table.remove(ARGV, 1)
+  local hash_key = table.remove(KEYS, 1)
+  redis.call('HMSET', hash_key, 'last_updated_at', last_updated_at, 'file_hash', file_hash)
+  redis.call('HSETNX', hash_key, 'first_updated_at', first_updated_at)
+  for i, values in ipairs(KEYS) do
     if ARGV[i] == '-1' then
-      redis.call("HSET", KEYS[1], KEYS[i+1], ARGV[i])
+      redis.call("HSET", hash_key, KEYS[i], ARGV[i])
     else
-      redis.call("HINCRBY", KEYS[1], KEYS[i+1], ARGV[i])
+      redis.call("HINCRBY", hash_key, KEYS[i], ARGV[i])
     end
   end
-  return redis.call('HGETALL', KEYS[1])
+  return redis.call('HGETALL', hash_key)
 LUA
-puts redis.evalsha(script_id, %w[family josie noah wendy daddy gracie yvette], [8, 1, 3, 5, 6, -1])
+
+debug_script_id = redis.script(:load, <<~LUA)
+  local first_updated_at = table.remove(ARGV, 1)
+  local last_updated_at = table.remove(ARGV, 1)
+  local file_hash = table.remove(ARGV, 1)
+  local hash_key = table.remove(KEYS, 1)
+  redis.call('HMSET', hash_key, 'last_updated_at', last_updated_at, 'file_hash', file_hash)
+  redis.call('HSETNX', hash_key, 'first_updated_at', first_updated_at)
+  return { first_updated_at, last_updated_at, file_hash, hash_key }
+LUA
+
+puts redis.evalsha(script_id, %w[family josie noah wendy daddy gracie yvette], ['first_updated_at', 'last_updated_at', 'file_hash', 8, 1, 3, 5, 6, -1])
