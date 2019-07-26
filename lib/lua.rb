9325a911416899c48fc7a1b1b17ce51ae93f8456
Karl Baum <karl.baum@gmail.com>
2019-07-25 20:50:13 -0400

    Script working

--- lib/lua.rb
+++ lib/lua.rb
@@ -5,7 +5,12 @@ require 'redis'
 redis = Redis.new
 script_id = redis.script(:load, <<~LUA)
   for i, values in ipairs(ARGV) do
-    redis.call("HINCRBY", KEYS[1], KEYS[i+1], ARGV[i])
+    if ARGV[i] == '-1' then
+      redis.call("HSET", KEYS[1], KEYS[i+1], ARGV[i])
+    else
+      redis.call("HINCRBY", KEYS[1], KEYS[i+1], ARGV[i])
+    end
   end
+  return redis.call('HGETALL', KEYS[1])
 LUA
-puts redis.evalsha(script_id, %w[family josie noah wendy daddy gracie], [8, 1, 3, 5, 6])
+puts redis.evalsha(script_id, %w[family josie noah wendy daddy gracie yvette], [8, 1, 3, 5, 6, -1])
