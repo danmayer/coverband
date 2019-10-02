e254ecb229a1f5531b0108a919095a7cd764143e
Karl Baum <karl.baum@gmail.com>
2019-10-02 18:45:27 -0400

    Move lua install to tmp dir

--- lua/install.sh
+++ lua/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+LUA_DIR="./tmp/lua51"
 pip install hererocks
-hererocks lua51 -l5.1 -rlatest
-source lua51/bin/activate
+hererocks $LUA_DIR -l5.1 -rlatest
+source $LUA_DIR/bin/activate
 lua -v
 for i in luacov busted redis-lua inspect; do 
   luarocks install $i;
