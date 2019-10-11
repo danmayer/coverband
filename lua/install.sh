1fc28cb49ac1c950e2ebf798734a0eaaab3d3e6a
Karl Baum <karl.baum@gmail.com>
2019-10-10 21:25:16 -0400

    Cache lua installation on travis

--- lua/install.sh
+++ lua/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-LUA_DIR="./tmp/lua51"
+LUA_DIR="$HOME/lua51"
+
+if [ -d "$LUA_DIR" ]
+then
+  echo "$LUA_DIR already exists"
+  exit 0
+fi
+
 pip install hererocks
 hererocks $LUA_DIR -l5.1 -rlatest
 source $LUA_DIR/bin/activate
