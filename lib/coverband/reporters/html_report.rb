b2f6a66b6bf6fc4c4d46dd704d13b9fcca029267
Dan Mayer <danmayer@gmail.com>
2020-08-23 18:14:04 -0600

    fixing non-ruby 2.3.x complaint standard recommendation

--- lib/coverband/reporters/html_report.rb
+++ lib/coverband/reporters/html_report.rb
@@ -7,7 +7,6 @@ module Coverband
         :base_path, :filename
 
       def initialize(store, options = {})
-        super(store, options = {})
         coverband_reports = Coverband::Reporters::Base.report(store, options)
         self.open_report = options.fetch(:open_report) { true }
         # TODO: refactor notice out to top level of web only
