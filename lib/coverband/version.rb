d4f9c6a3ad353c16e53ed4bb1a38859d879b28fe
Dan Mayer <danmayer@gmail.com>
2020-12-31 16:55:04 -0700

    add null store for brenchmarking and fix standard RB issue

--- lib/coverband/version.rb
+++ lib/coverband/version.rb
@@ -2,8 +2,8 @@
 
 ###
 # ensure we properly do release candidate versioning; https://github.com/danmayer/coverband/issues/288
-# use format '4.2.1.rc.1' ~> 4.2.1.rc to prerelease versions like v4.2.1.rc.2 and v4.2.1.rc.3
+# use format "4.2.1.rc.1" ~> 4.2.1.rc to prerelease versions like v4.2.1.rc.2 and v4.2.1.rc.3
 ###
 module Coverband
-  VERSION = '5.1.0'
+  VERSION = "5.1.0"
 end
