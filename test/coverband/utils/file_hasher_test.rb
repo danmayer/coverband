24be62fbd41a93793828f8d15f6d2643d3553c13
Dan Mayer <dan.mayer@stitchfix.com>
2023-05-09 22:11:35 -0600

    address issue #478 by renaming hash

--- test/coverband/utils/file_hasher_test.rb
+++ test/coverband/utils/file_hasher_test.rb
@@ -6,17 +6,17 @@ module Coverband
   module Utils
     class FileHasherTest < Minitest::Test
       def test_hash_same_file
-        refute_nil FileHasher.hash("./test/dog.rb")
-        assert_equal(FileHasher.hash("./test/dog.rb"), FileHasher.hash("./test/dog.rb"))
-        assert_equal(FileHasher.hash(File.expand_path("./test/dog.rb")), FileHasher.hash("./test/dog.rb"))
+        refute_nil FileHasher.hash_file("./test/dog.rb")
+        assert_equal(FileHasher.hash_file("./test/dog.rb"), FileHasher.hash_file("./test/dog.rb"))
+        assert_equal(FileHasher.hash_file(File.expand_path("./test/dog.rb")), FileHasher.hash_file("./test/dog.rb"))
       end
 
       def test_hash_different_files
-        refute_equal(FileHasher.hash("./test/dog.rb"), FileHasher.hash("./lib/coverband.rb"))
+        refute_equal(FileHasher.hash_file("./test/dog.rb"), FileHasher.hash_file("./lib/coverband.rb"))
       end
 
       def test_hash_file_not_exists
-        assert_nil(FileHasher.hash("./made_up_file.py"))
+        assert_nil(FileHasher.hash_file("./made_up_file.py"))
       end
     end
   end
